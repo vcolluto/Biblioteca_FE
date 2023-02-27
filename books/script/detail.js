82f91bcc89f07c5fcc426f0075366a0d531016a4
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-02-27 12:45:19 +0100

    pagina dettaglio (da finire)

---
+++ books/script/detail.js
@@ -0,0 +1,15 @@
+const URLParams=new URLSearchParams(window.location.search);
+const bookId=URLParams.get('id');
+
+axios.get(`http://localhost:8080/books/${bookId}`).then((res) => {
+    console.log('richiesta ok', res);
+    //res.data è un Book 
+    document.querySelector('#title').innerHTML= res.data.title;
+    document.querySelector('#author').innerHTML= res.data.author;
+    document.querySelector('#isbn').innerHTML= res.data.isbn;
+    document.querySelector('#availableCopies').innerHTML= res.data.isbn;
+    
+}).catch((res) => {
+    console.error('errore nella richiesta', res);
+    alert('Errore durante la richiesta!');
+});
