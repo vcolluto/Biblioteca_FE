7428f87a666b76767bd19a6e06e40ee8ea3c6169
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-03-01 10:41:59 +0100

    Aggiunta visualizzazione categorie su detail

--- books/script/detail.js
+++ books/script/detail.js
@@ -7,7 +7,11 @@ axios.get(`http://localhost:8080/books/${bookId}`).then((res) => {
     document.querySelector('#title').innerHTML= res.data.title;
     document.querySelector('#author').innerHTML= res.data.author;
     document.querySelector('#isbn').innerHTML= res.data.isbn;
-    document.querySelector('#availableCopies').innerHTML= res.data.isbn;
+    document.querySelector('#availableCopies').innerHTML= res.data.availableCopies;
+    res.data.categories.forEach(category => {
+        document.querySelector('#ul_categories').innerHTML+=
+        '<li>'+category.name+'</li>';
+    });
     
 }).catch((res) => {
     console.error('errore nella richiesta', res);
