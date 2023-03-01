e7f0306ee5175170e00df257db055d4d1386c2cb
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-03-01 10:00:37 +0100

    Gestione errori validazione su create

--- books/script/create.js
+++ books/script/create.js
@@ -1,13 +1,40 @@
 function createBook(event) {
-    axios.post('http://localhost:8080/books', {
-        title:document.querySelector('#title').value,
-        author:document.querySelector('#author').value,
-    }).then((res) => {
+    event.preventDefault();
 
-        //per gestire errori di valutazione: 
-       // res.response.data.errors
+    const book={
+        title : document.querySelector('#title').value,
+        author : document.querySelector('#author').value,
+        isbn : document.querySelector('#isbn').value,
+        availableCopies: document.querySelector('#availableCopies').value
+    };
 
+    
+    axios.post('http://localhost:8080/books', book).then((res) => {
+        console.log("inserimento ok");
+        location.href="./index.html";
+     
     }).catch((res) => {
+        console.error("errore nell'inserimento",res);
+        showValidationErrors(res.response.data.errors);
+    })
+
+}
+
+function showValidationErrors(errorList) {
+    console.error("errori validazione",errorList);
+
+    resetValidationErrors();
+    errorList.forEach(error => {
+        document.querySelector("#validation_errors").innerHTML+="<li>"+error.defaultMessage+"</li>";
+        document.querySelector(`#${error.field}_err`).innerHTML+="<li>"+error.defaultMessage+"</li>";
+    });
+}
+
+
+function resetValidationErrors() {
+    document.querySelector("#validation_errors").innerHTML="";
+    document.querySelectorAll("[id$=_err]").forEach(element => {    //tutti gli id che finiscono con _err
+        element.innerHTML="";
     })
 
 }
